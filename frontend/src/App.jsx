6708dcf8f531a864bf4ffdf5f1e0c53cda7510e8
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2024-12-10 20:11:31 +0530

    talwindcss sucessfully configred

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-red-500"> Uber colne</h1>
+    </>
+  );
+}
+
+export default App;
